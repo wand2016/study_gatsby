a52387b2cbfb575a9a5fad59b3f6b4a09ee0dd89
wand2016 <d19921207@gmail.com>
2020-10-29 09:09:44 +0900

    年次/月次記事一覧

---
+++ src/templates/date-filtered-posts.tsx
@@ -0,0 +1,66 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Posts from "@/components/posts"
+import Bio from "@/components/bio"
+import Layout from "@/components/layout"
+import SEO from "@/components/seo"
+import moment from "moment"
+
+type Props = {
+  data: GatsbyTypes.DateFilteredPostsQuery
+  pageContext: GatsbyTypes.SitePageContext
+  location: Location
+}
+const DateFilteredPostsIndex: React.FC<Props> = ({
+  data,
+  pageContext,
+  location,
+}) => {
+  const siteTitle = data?.site?.siteMetadata?.title ?? `no siteTitle`
+  const posts = data?.allMarkdownRemark?.nodes
+  const periodStart = moment(pageContext.periodStart)
+  const periodEnd = moment(pageContext.periodEnd)
+
+  const period = `${periodStart.format("YYYY/MM/DD")} - ${periodEnd.format(
+    "YYYY/MM/DD"
+  )}`
+  const seoTitle = `posts during ${period}`
+
+  return (
+    <Layout location={location} title={siteTitle}>
+      <SEO title={seoTitle} />
+      <Bio />
+      {seoTitle}
+      <Posts posts={posts} />
+    </Layout>
+  )
+}
+
+export default DateFilteredPostsIndex
+
+export const pageQuery = graphql`
+  query DateFilteredPosts($periodStart: Date!, $periodEnd: Date!) {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+      filter: { frontmatter: { date: { gte: $periodStart, lte: $periodEnd } } }
+    ) {
+      totalCount
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date
+          title
+          tags
+        }
+      }
+    }
+  }
+`
