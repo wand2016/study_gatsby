5cbd6452547ce54398d6397d117d63f53b2ab125
wand2016 <d19921207@gmail.com>
2020-11-15 23:38:32 +0900

    タグ一覧でタグごとの投稿件数表示

--- src/pages/tags.tsx
+++ src/pages/tags.tsx
@@ -12,23 +12,26 @@ type Props = {
 const NotFoundPage: React.FC<Props> = ({ data, location }) => {
   const siteTitle = data?.site?.siteMetadata?.title ?? "no title"
   const posts = data?.allMarkdownRemark.nodes ?? []
-  const tags = [
-    ...new Set(
-      posts.reduce((tags: string[], post) => {
-        const nodeTags = (post?.frontmatter?.tags ?? []).filter(isJust)
-        return tags.concat(nodeTags)
-      }, [])
-    ),
-  ].sort()
+
+  const tagCounts: Record<string, number> = {}
+  for (const post of posts) {
+    const nodeTags = (post?.frontmatter?.tags ?? []).filter(isJust)
+    for (const nodeTag of nodeTags) {
+      tagCounts[nodeTag] = (tagCounts[nodeTag] ?? 0) + 1
+    }
+  }
+  const tags = Object.keys(tagCounts).sort()
 
   return (
     <Layout location={location} title={siteTitle}>
-      <SEO title="All Tags" />
+      <SEO title="タグ別記事" />
       <h1>タグ一覧</h1>
       <ul>
         {tags.map(tag => (
           <li>
-            <Link to={`/tags/${tag}`}>{tag}</Link>
+            <Link to={`/tags/${tag}`}>
+              {tag} ({tagCounts[tag]})
+            </Link>
           </li>
         ))}
       </ul>
