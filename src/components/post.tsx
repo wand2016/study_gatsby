a88fdc7d9ac600aff8e2600833659cc6bbf14a76
wand2016 <d19921207@gmail.com>
2021-01-24 05:14:34 +0900

    記事の見た目修正

--- src/components/post.tsx
+++ src/components/post.tsx
@@ -2,14 +2,17 @@ import React from "react"
 import Tags from "@/components/tags"
 import { isJust } from "@/utils/assertions"
 import DatetimeBreadCrumb from "@/components/datetime-bread-crumb"
+import styled from "styled-components"
+import { Card } from "primereact/card"
 
 type Just<T> = T extends undefined ? never : T
 type Post = Just<GatsbyTypes.BlogPostBySlugQuery["markdownRemark"]>
 type Props = {
+  className?: string
   post: Post
 }
 
-const Post: React.FC<Props> = ({ post }) => {
+const Post: React.FC<Props> = ({ className, post }) => {
   const tags = (post.frontmatter?.tags ?? []).filter(isJust)
 
   const bibliographies = post.frontmatter?.bibliography
@@ -17,41 +20,50 @@ const Post: React.FC<Props> = ({ post }) => {
     : post.frontmatter?.bibliographies ?? []
 
   return (
-    <article
-      className="markdown-body"
-      itemScope
-      itemType="http://schema.org/Article"
-    >
-      <header>
-        {post.frontmatter?.date ? (
-          <DatetimeBreadCrumb date={post.frontmatter?.date} />
-        ) : null}
-        <h1 itemProp="headline">{post.frontmatter?.title}</h1>
-      </header>
-      <Tags tags={tags} />
-      {bibliographies.length ? (
-        <section>
-          <h2>出典:&nbsp;</h2>
-          <ul>
-            {bibliographies.map((bibliography, i) => (
-              <li key={`bibliography-${i}`}>
-                <a href={bibliography} target="_blank">
-                  {bibliography}
-                </a>
-              </li>
-            ))}
-          </ul>
-        </section>
+    <>
+      {post.frontmatter?.date ? (
+        <DatetimeBreadCrumb date={post.frontmatter?.date} />
       ) : null}
-      <hr />
-      <section
-        dangerouslySetInnerHTML={{
-          __html: post.html ?? "no content",
-        }}
-        itemProp="articleBody"
-      />
-    </article>
+
+      <article
+        className={className}
+        itemScope
+        itemType="http://schema.org/Article"
+      >
+        <section className="p-p-3">
+          <h1 itemProp="headline">{post.frontmatter?.title}</h1>
+          <Tags tags={tags} />
+          {bibliographies.length ? (
+            <section>
+              <h2>出典:&nbsp;</h2>
+              <ul>
+                {bibliographies.map((bibliography, i) => (
+                  <li key={`bibliography-${i}`}>
+                    <a href={bibliography} target="_blank">
+                      {bibliography}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </section>
+          ) : null}
+        </section>
+        <Card>
+          <section
+            className="markdown-body"
+            dangerouslySetInnerHTML={{
+              __html: post.html ?? "no content",
+            }}
+            itemProp="articleBody"
+          />
+        </Card>
+      </article>
+    </>
   )
 }
 
-export default Post
+export default styled(Post)`
+  .markdown-body {
+    background-color: var(--surface-a);
+  }
+`
